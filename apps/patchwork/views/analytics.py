4ecb6dc8cbee50e587ad5b8734886e76ce43f72a
Deen Sethanandha <deenseth@gmail.com>
2010-08-04 02:23:39 +0700

    Create new file to handle analytics feature.
    Signed-off-by: Deen Sethanandha <deenseth@gmail.com>

--- apps/patchwork/views/analytics.py
+++ apps/patchwork/views/analytics.py
@@ -1,5 +1,5 @@
 # Patchwork - automated patch tracking system
-# Copyright (C) 2009 Jeremy Kerr <jk@ozlabs.org>
+# Copyright (C) 2010 Deen Sethanandha <deenseth@gmail.com>
 #
 # This file is part of the Patchwork package.
 #
@@ -24,14 +24,29 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from patchwork.requestcontext import PatchworkRequestContext
 
-def project(request, project_id):
+def dashboard(request, project_id):
     context = PatchworkRequestContext(request)
     project = get_object_or_404(Project, linkname = project_id)
     context.project = project
 
-    context['maintainers'] = User.objects.filter( \
-            userprofile__maintainer_projects = project)
     context['n_patches'] = Patch.objects.filter(project = project,
             archived = False).count()
+    context['n_archived_patches'] = Patch.objects.filter(project = project,
+            archived = True).count()
+    context['n_new_patches'] = Patch.objects.filter(project = project,
+            archived = False, state = 1).count()
+    context['n_underreview_patches'] = Patch.objects.filter(project = project,
+            archived = False, state = 2).count()
+    context['n_accepted_patches'] = Patch.objects.filter(project = project,
+            archived = False, state = 3).count()
+    context['n_rejected_patches'] = Patch.objects.filter(project = project,
+            archived = False, state = 4).count()
+    context['n_resolved_patches'] = context['n_accepted_patches'] + \
+                                    context['n_rejected_patches']
+    
+    context['n_reviewers'] = Person.objects.filter(comment__patch__project = project).distinct().count()
+    context['n_contributors'] = Person.objects.filter(patch__project = project).distinct().count()
 
-    return render_to_response('patchwork/project.html', context)
+    return render_to_response('patchwork/analytics.html', context)
+
+
